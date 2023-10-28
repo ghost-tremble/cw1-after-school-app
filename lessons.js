18edaafc0027a1646db68be2ebb1c08fa4f5262f
ghost-tremble <=>
2023-10-28 17:32:55 +0100

    created lessons list UI

---
+++ lessons.js
@@ -0,0 +1,94 @@
+// Ten lessons required here
+let lessons = [
+  {
+    subject: "Bible Studies",
+    spaces: 5,
+    location: "Barnet",
+    price: 500,
+    image:
+      "https://images.unsplash.com/photo-1442115597578-2d0fb2413734?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fENocmlzdGlhbiUyMGVkdWNhdGlvbiUyMGNvdXJzZXxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    subject: "French Tutorial",
+    spaces: 5,
+    location: "liverpool",
+    price: 250,
+    image:
+      "https://images.unsplash.com/photo-1505902987837-9e40ec37e607?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZnJlbmNofGVufDB8fDB8fHww",
+  },
+  {
+    subject: "Japanese Tutorial",
+    spaces: 5,
+    location: "leeds",
+    price: 750,
+    image:
+      "https://images.unsplash.com/photo-1492998680170-81f8863d8d2d?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGphcGFuZXNlJTIwdHV0b3JpYWx8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    subject: "Python Coding Tutorial",
+    spaces: 5,
+    location: "watford",
+    price: 150,
+    image:
+      "https://images.unsplash.com/photo-1557324232-b8917d3c3dcb?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHB5dGhvbiUyMHByb2dyYW1taW5nfGVufDB8fDB8fHww",
+  },
+
+  {
+    subject: "History",
+    spaces: 5,
+    location: "north london",
+    price: 520,
+    image:
+      "https://images.unsplash.com/photo-1479142506502-19b3a3b7ff33?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aGlzdG9yeXxlbnwwfHwwfHx8MA%3D%3D",
+  },
+
+  {
+    subject: "Algebra",
+    spaces: 5,
+    location: "Kilburn",
+    price: 450,
+    image:"https://images.unsplash.com/photo-1653074281018-c08f358059ab?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGFsZ2VicmF8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    subject: "Javascript Coding tutorial",
+    spaces: 5,
+    location: "Archway",
+    price: 650,
+    image:
+      "https://images.unsplash.com/photo-1517512006864-7edc3b933137?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fEphdmFzY3JpcHQlMjBDb2RpbmclMjB0dXRvcmlhbHxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    subject: "Chemistry",
+    spaces: 5,
+    location: "Watford",
+    price: 550,
+    image:
+      "https://images.unsplash.com/photo-1532094349884-543bc11b234d?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hlbWlzdHJ5fGVufDB8fDB8fHww",
+  },
+
+  {
+    subject: "Economics",
+    spaces: 5,
+    location: "machester",
+    price: 350,
+    image:
+      "https://images.unsplash.com/photo-1612178991541-b48cc8e92a4d?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZWNvbm9taWNzfGVufDB8fDB8fHww",
+  },
+  {
+    subject: "Bussiness ethics",
+    spaces: 5,
+    location: "burnley",
+    price: 150,
+    image:
+      "https://images.unsplash.com/photo-1431540015161-0bf868a2d407?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGJ1c2luZXNzJTIwZXRoaWNzfGVufDB8fDB8fHww",
+  },
+
+  {
+    subject: "Health and fitness",
+    spaces: 5,
+    location: "bournemouth",
+    price: 250,
+    image:
+      "https://images.unsplash.com/photo-1551984427-6d77a1918093?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aGVhbHRoJTIwYW5kJTIwZml0bmVzc3xlbnwwfHwwfHx8MA%3D%3D",
+  },
+];
